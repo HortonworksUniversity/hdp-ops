4947770f1d8e8497e270ff25a1a535a59bffd92a
Michael Bingham <michael.b.bingham@gmail.com>
2015-07-10 20:44:05 -0700

    Update prepare_env.sh

--- blueprints/prepare_env.sh
+++ blueprints/prepare_env.sh
@@ -4,11 +4,11 @@ cur=$PWD
 
 cd /root/scripts
 ./env_setup.sh
-cd $cur
 yum -y install ambari-server
 ./copy_jdk.sh
 ambari-server setup -s
 ambari-server start
+cd $cur
 
 ssh root@node1 bash <<EOF
 yum -y install ambari-agent
